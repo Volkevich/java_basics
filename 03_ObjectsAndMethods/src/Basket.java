a88a8a621e6ff0dc92ca19b31337fcb6e3f8a6e3
Виталий <Vinderosv@yandex.by>
2021-08-18 00:34:28 +0300

    Готово

--- 03_ObjectsAndMethods/src/Basket.java
+++ 03_ObjectsAndMethods/src/Basket.java
@@ -1,15 +1,22 @@
 public class Basket {
 
     private static int count = 0;
-    private String items = "";
-    private int totalPrice = 0;
+    private static int totalCount = 0;
+    private static String items = " ";
+    private static int totalPrice = 0;
     private int limit;
+    public double totalWeight = 0;
+
 
     public Basket() {
         increaseCount(1);
         items = "Список товаров:";
         this.limit = 1000000;
+
+
     }
+
+
 
     public Basket(int limit) {
         this();
@@ -18,8 +25,8 @@ public class Basket {
 
     public Basket(String items, int totalPrice) {
         this();
-        this.items = this.items + items;
-        this.totalPrice = totalPrice;
+        Basket.items = Basket.items + items;
+        Basket.totalPrice = totalPrice;
     }
 
     public static int getCount() {
@@ -29,10 +36,14 @@ public class Basket {
     public static void increaseCount(int count) {
         Basket.count = Basket.count + count;
     }
+    public static int getTotalCount() {
+        return Basket.totalCount;
+    }
 
     public void add(String name, int price) {
         add(name, price, 1);
     }
+
 
     public void add(String name, int price, int count) {
         boolean error = false;
@@ -40,7 +51,7 @@ public class Basket {
             error = true;
         }
 
-        if (totalPrice + count * price >= limit) {
+        if (Basket.totalPrice + count * price >= limit) {
             error = true;
         }
 
@@ -49,23 +60,49 @@ public class Basket {
             return;
         }
 
-        items = items + "\n" + name + " - " +
-            count + " шт. - " + price;
-        totalPrice = totalPrice + count * price;
+        items = items + "\n" + name + " - " + price + " руб.";
+       Basket.totalPrice = Basket.totalPrice + count * price;
+
     }
+
+
+    public void add(String name, int price, int count, double weight)
+    {
+        add(name,price);
+        totalWeight = totalWeight + weight;
+        totalCount = totalCount + count;
+        items = items +  " " + count + " шт. " + " весом - " + weight + " грамм";
+
+    }
+
+
 
     public void clear() {
         items = "";
         totalPrice = 0;
     }
 
-    public int getTotalPrice() {
+    public double getTotalWeight()
+    {
+        return totalWeight;
+    }
+
+    public static double getTotalPrice() {
         return totalPrice;
     }
+    public static double averagePrice() {
+        return getTotalPrice() / getTotalCount();
+    }
+
+    public static double averageBasket() {
+        return getTotalPrice() / Basket.getCount();
+    }
+
 
     public boolean contains(String name) {
         return items.contains(name);
     }
+
 
     public void print(String title) {
         System.out.println(title);
@@ -73,6 +110,7 @@ public class Basket {
             System.out.println("Корзина пуста");
         } else {
             System.out.println(items);
+            System.out.println("Общий вес корзины: " + getTotalWeight() + " граммм");
         }
     }
 }
--- 03_ObjectsAndMethods/src/Main.java
+++ 03_ObjectsAndMethods/src/Main.java
@@ -1,8 +1,28 @@
 public class Main {
 
     public static void main(String[] args) {
-        Basket basket = new Basket();
-        basket.add("Milk", 40);
-        basket.print("Milk");
+        Basket basketVasia = new Basket();
+        basketVasia.add("Milk", 10);
+        basketVasia.print("Корзина Васи");
+        System.out.println("//-----------------------------------//");
+        Basket basketMax = new Basket();
+        basketMax.add("Apple", 10, 3, 20);
+        basketMax.add("Cucumber", 10, 3, 33);
+        basketMax.add("Лимон", 2, 3, 30);
+        basketMax.add("Персик", 10, 3, 30);
+        basketMax.print("Корзина Макса - ");
+        System.out.println("=========================================================================");
+        System.out.println("Общая сумма товаров со всех корзин: " + Basket.getTotalPrice() + " руб.");
+        Basket basketMaxx = new Basket();
+        basketMaxx.add("Apple", 2, 3, 20);
+        System.out.println("Количество корзин: " + Basket.getCount());
+        System.out.println("Количество товаров: " + Basket.getTotalCount() + " шт.");
+        System.out.println("Средняя цена: " + Basket.averagePrice() + " руб.");
+        System.out.println("Средняя стоимость корзины: " + Basket.averageBasket() + " руб.");
+
+
+
+
+
     }
 }
