eaa663a18c12f993b8fadcd34e602bd579a105f7
Виталий Власов <vinderosv@yandex.by>
2021-08-24 18:42:46 +0000

    Update Main.java

--- 05_StaticFinal/02_enum/src/ru/skillbox/Main.java
+++ 05_StaticFinal/02_enum/src/ru/skillbox/Main.java
@@ -1,10 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        ArithmeticCalculator arithmeticCalculator = new ArithmeticCalculator(5,2);
-        arithmeticCalculator.calculate();
-        System.out.println(Operation.ADD);
-        System.out.println(Operation.MULTIPLY);
-        System.out.println(Operation.SUBTRACT);
+        ArithmeticCalculator calculator = new ArithmeticCalculator(2,8);
+        calculator.calculate(Operation.ADD);
+        calculator.calculate(Operation.SUBTRACT);
+        calculator.calculate(Operation.MULTIPLY);
+        System.out.println(calculator.calculate(Operation.ADD));
+        System.out.println(calculator.calculate(Operation.SUBTRACT));
+        System.out.println(calculator.calculate(Operation.MULTIPLY));
 
     }
 }
