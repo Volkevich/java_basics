539e44c8e184054bdc04d745bcb7c49327a47221
Vinderosv <Vinderosv@yandex.by>
2021-09-04 21:36:26 +0300

    Сделал как смог, направьте на путь истинный ))

--- 07_NumbersAndDates/TrucksContainersAndBoxes/src/main/java/Main.java
+++ 07_NumbersAndDates/TrucksContainersAndBoxes/src/main/java/Main.java
@@ -1,10 +1,42 @@
 import java.util.Scanner;
 
 public class Main {
+    public static final int BOXES_IN_CONTAINER = 27;
+    public static final int CONTAINERS_IN_TRUCKS = 12;
+    public static int containerNumber = 1;
+    public static int truckNumber = 1;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите количество ящиков: ");
         String boxes = scanner.nextLine();
+        int boxesCount = Integer.parseInt(boxes);
+
+        if (boxesCount != 0) {
+            System.out.println("Грузовик: " + truckNumber);
+            System.out.println("\tКонтейнер: " + containerNumber);
+            for (int i = 1; i <= boxesCount; i++) {
+                System.out.println("\t\tЯщик: " + i);
+                if (i % (BOXES_IN_CONTAINER * CONTAINERS_IN_TRUCKS) == 0) {
+
+                    truckNumber += 1;
+                    System.out.println("Грузовик: " + truckNumber);
+                }
+                if (i % BOXES_IN_CONTAINER == 0) {
+                    containerNumber += 1;
+                    System.out.println("\tКонтейнер: " + containerNumber);
+                }
+            } System.out.println("Необходимо: "+System.lineSeparator()+"\tгрузовиков: "+ truckNumber +" шт."+System.lineSeparator()+"\tконтейнеров: "
+                    + containerNumber +" шт.");
+
+
+        }else System.out.println("Необходимо: "+System.lineSeparator()+"\tгрузовиков: "+ --truckNumber +" шт."+System.lineSeparator()+"\tконтейнеров: "
+                + --containerNumber +" шт.");
+    }
+    }
+
+
+
 
         // TODO: вывести в консоль коробки разложенные по грузовикам и контейнерам
         // пример вывода при вводе 2
@@ -19,6 +51,6 @@ public class Main {
         грузовиков - 1 шт.
         контейнеров - 1 шт.
         */
-    }
 
-}
+
+
