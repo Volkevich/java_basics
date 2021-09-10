d01b9857bc0ce3887f50f98590f6e0c338f6b6d0
Vinderosv <Vinderosv@yandex.by>
2021-09-10 22:29:43 +0300

    в процессе

--- 07_NumbersAndDates/Periods/src/main/java/Main.java
+++ 07_NumbersAndDates/Periods/src/main/java/Main.java
@@ -1,13 +1,30 @@
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
 public class Main {
 
   public static void main(String[] args) {
 
-  }
+    LocalDate birthday = LocalDate.of(1970,1,1);
+    System.out.println(getPeriodFromBirthday(birthday));
 
-  private static String getPeriodFromBirthday(LocalDate birthday) {
-    return "";
   }
+  public static String getPeriodFromBirthday(LocalDate birthday){
+
+      LocalDate now = LocalDate.now();
+      String years = "";
+      years = years + ChronoUnit.YEARS.between(now,birthday);
+    String day = "";
+    day = day + ChronoUnit.DAYS.between(now,birthday);
+    String month = "";
+    month = month + ChronoUnit.MONTHS.between(now,birthday);
+      String period = "";
+      period = period + years + month + day;
+
+
+      return period;
+    }
+
 
 }
