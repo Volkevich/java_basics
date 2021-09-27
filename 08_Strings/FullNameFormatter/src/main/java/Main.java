2cf38655f0965e23fa485fcf7d9e969772015fbe
Vinderosv <Vinderosv@yandex.by>
2021-09-27 10:30:25 +0300

    Готово.

--- 08_Strings/FullNameFormatter/src/main/java/Main.java
+++ 08_Strings/FullNameFormatter/src/main/java/Main.java
@@ -8,51 +8,40 @@ public class Main {
     String iotName = "";
     String otcName = "";
     int spaceCount = 0;
-
+    int dashCount = 0;
     Scanner scanner = new Scanner(System.in);
     String fio = scanner.nextLine();
-
-
-
-
-    while (true) {
-
-      for (int i = 0; i < fio.length(); i++) {
-
-        char fioSymbol = fio.charAt(i);
-        if (fioSymbol == ' ') {
-          spaceCount++;
-        }
+    for (int i = 0; i < fio.length(); i++) {
+      char fioSymbol = fio.charAt(i);
+      if (fioSymbol == ' ') {
+        spaceCount++;
+      }else if (fioSymbol == '-'){
+        dashCount++;
       }
-      for (int i = 0; i < fio.length(); i++) {
-        char fioSymbol = fio.charAt(i);
-        if (!Character.UnicodeBlock.of(fioSymbol).equals(Character.UnicodeBlock.CYRILLIC)) {
-          System.out.println("Введенная строка не является ФИО");
-          break;
-        }
-        if (Character.isDigit(fioSymbol)){
-          System.out.println("Введенная строка не является ФИО");
-          break;
-        }
-
-        if (spaceCount < 3){
-          int end = fio.indexOf(" ");
-          firstName = fio.substring(i,end);
-
-
-          String template = "Фамилия: %s\nИмя: %s\nОтчество: %s\n";
-          System.out.println(firstName + otcName);
-          System.out.println(spaceCount);
-
-        }else {
-          System.out.println("Введенная строка не является ФИО");
-        }
-        break;
-      }
-      break;
+      else if (((!Character.UnicodeBlock.of(fioSymbol).equals(Character.UnicodeBlock.CYRILLIC)) || (Character.isDigit(fioSymbol)))){
+        System.out.println("Введенная строка не является ФИО");
+        return;
       }
     }
+
+      if ((spaceCount == 2 && dashCount == 1) || (spaceCount == 2 && dashCount == 0) ) {
+        firstName = fio.substring(0, fio.indexOf(" "));
+        fio = fio.substring(fio.indexOf(" ") + 1);
+        iotName = fio.substring(0,fio.indexOf(" "));
+        fio = fio.substring(fio.indexOf(" ") + 1);
+        otcName = fio;
+
+
+        String template = "Фамилия: %s\nИмя: %s\nОтчество: %s\n";
+        System.out.printf(template,firstName,iotName,otcName);
+
+      }else {
+        System.out.println("Введенная строка не является ФИО");
+      }
+
+    }
   }
+
 
 //TODO:напишите ваш код тут, результат вывести в консоль.
 //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
