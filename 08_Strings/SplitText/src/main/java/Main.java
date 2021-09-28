3e6ca9ea362492554c0c34da92cd48073555f8f6
Vinderosv <Vinderosv@yandex.by>
2021-09-28 14:08:28 +0300

    Готово.

--- 08_Strings/SplitText/src/main/java/Main.java
+++ 08_Strings/SplitText/src/main/java/Main.java
@@ -2,9 +2,23 @@ public class Main {
 
   public static void main(String[] args) {
 
+
+    String text = "I didn’t think, I didn’t guess,\n" +
+            "He took it and became a programmer.\n" +
+            "Sly he knows the language,\n" +
+            "He's not used to anything else.";
+    System.out.println(splitTextIntoWords(text));
+
+
   }
 
   public static String splitTextIntoWords(String text) {
+    String[] words = text.split("[\\s,\\.]+".trim());
+    for (int i = 0; i < words.length;i++){
+      System.out.println(words[i]);
+    }
+
+
     //TODO реализуйте метод
     return "";
   }
