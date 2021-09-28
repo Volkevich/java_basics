938d8ac828547ca2c617adc16ef12d035d15e07d
Vinderosv <Vinderosv@yandex.by>
2021-09-28 16:54:58 +0300

    Готово.

--- 08_Strings/FullNameFormatterRegExp/src/main/java/Main.java
+++ 08_Strings/FullNameFormatterRegExp/src/main/java/Main.java
@@ -3,16 +3,32 @@ import java.util.Scanner;
 public class Main {
 
   public static void main(String[] args) {
-
+    int spaceCount = 0;
+    int dashCount = 0;
     Scanner scanner = new Scanner(System.in);
-    while (true) {
-      String input = scanner.nextLine();
-      if (input.equals("0")) {
-        break;
+    String fio = scanner.nextLine();
+    for (int i = 0; i < fio.length(); i++) {
+      char fioSymbol = fio.charAt(i);
+      if (fioSymbol == ' ') {
+        spaceCount++;
+      }else if (fioSymbol == '-'){
+        dashCount++;
       }
-      //TODO:напишите ваш код тут, результат вывести в консоль.
-      //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
+      else if (((!Character.UnicodeBlock.of(fioSymbol).equals(Character.UnicodeBlock.CYRILLIC)) || (Character.isDigit(fioSymbol)))){
+        System.out.println("Введенная строка не является ФИО");
+        return;
+      }
     }
-  }
 
+    if ((spaceCount == 2 && dashCount == 1) || (spaceCount == 2 && dashCount == 0) ) {
+      String[]fio1 = fio.split(" ");
+
+      String template = "Фамилия: %s\nИмя: %s\nОтчество: %s\n";
+      System.out.printf(template,fio1[0],fio1[1],fio1[2]);
+
+    }else {
+      System.out.println("Введенная строка не является ФИО");
+    }
+
+  }
 }
