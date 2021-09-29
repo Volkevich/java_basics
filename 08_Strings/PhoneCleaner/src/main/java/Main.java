d53db8bd79ad4bb17fd8c118bb16503d2a160082
Vinderosv <Vinderosv@yandex.by>
2021-09-29 15:38:09 +0300

    Готово.

--- 08_Strings/PhoneCleaner/src/main/java/Main.java
+++ 08_Strings/PhoneCleaner/src/main/java/Main.java
@@ -7,8 +7,26 @@ public class Main {
     Scanner scanner = new Scanner(System.in);
     while (true) {
       String input = scanner.nextLine();
-      if (input.equals("0")) {
-        break;
+      String regex = "[^0-9]";
+      String text = input.replaceAll(regex, "");
+
+      if (text.length()==10||text.length()==11) {
+        if (text.length()==10){
+          text = "7"+text;
+          System.out.print(text);
+          break;
+        }
+        else if (text.length()==11){
+          if(text.startsWith("7") || text.startsWith("8")){
+            if(text.startsWith("8"))
+              text = text.replaceFirst("8","7");
+            System.out.print(text);
+            return;
+          }
+        }
+      }else {
+        System.out.print("Неверный формат номера");
+        return;
       }
       //TODO:напишите ваш код тут, результат вывести в консоль.
     }
