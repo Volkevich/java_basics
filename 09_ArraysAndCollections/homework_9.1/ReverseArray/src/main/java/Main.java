5482faec9796cf71819372e7d9aaabb811e76ff6
Vinderosv <Vinderosv@yandex.by>
2021-10-10 15:24:14 +0300

    Готово.

--- 09_ArraysAndCollections/homework_9.1/ReverseArray/src/main/java/Main.java
+++ 09_ArraysAndCollections/homework_9.1/ReverseArray/src/main/java/Main.java
@@ -1,6 +1,14 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
         String line = "Каждый охотник желает знать, где сидит фазан";
+        String[] input = line.split(" ");
+        ReverseArray.reverse(input);
+        System.out.print(Arrays.toString(input));
+
+
     }
-}
+    }
--- 09_ArraysAndCollections/homework_9.1/ReverseArray/src/main/java/ReverseArray.java
+++ 09_ArraysAndCollections/homework_9.1/ReverseArray/src/main/java/ReverseArray.java
@@ -1,7 +1,21 @@
+import net.sf.saxon.expr.Component;
+
+import java.util.Arrays;
+import java.util.Collection;
+
 public class ReverseArray {
 
     //TODO: Напишите код, который меняет порядок расположения элементов внутри массива на обратный.
     public static String[] reverse (String[] strings){
+        String[] copy = Arrays.copyOf(strings,strings.length);
+        int index = 0;
+        for (int i = copy.length -1; i >=0; i--){
+            strings[index] = copy[i];
+            index++;
+        }
+
+
         return strings;
     }
+
 }
