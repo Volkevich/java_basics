e044f03e22ead8571da23a79e7e1f46df263814b
Виталий Власов <vinderosv@yandex.by>
2021-10-16 12:55:20 +0000

    Update TwoDimensionalArray.java

--- 09_ArraysAndCollections/homework_9.1/TwoDimensionalArray/src/main/java/TwoDimensionalArray.java
+++ 09_ArraysAndCollections/homework_9.1/TwoDimensionalArray/src/main/java/TwoDimensionalArray.java
@@ -8,15 +8,10 @@ public class TwoDimensionalArray {
     public static char[][] getTwoDimensionalArray(int size) {
         char[][]ris = new char[size][size];
         for (int i = 0; i < size;i++){
-            for (int j = 0; j < size;j++){
-                if ((i == 0 && j == 0) || (i == 0 && j == (size-1))||(i == size-1 && j == 0)||((i == (size-1)) && j == (size-1)) ||
-                        ((i==((size-1)/2) && j == ((size-1)/2)))) {
-                    ris[i][j] = symbol;
-                }
-                else
-                    ris[i][j] = ' ';
-                System.out.print(ris[i][j]);
+            for (int j = 0; j <size;j++){
+                ris[i][j] = (i == j || (j==size-i-1) ? symbol : ' ');
 
+                System.out.print(ris[i][j]);
             }
             System.out.println();
 
