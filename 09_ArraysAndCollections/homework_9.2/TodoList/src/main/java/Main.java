85e278d44bb9b70f572deb1f7a4ec51f1d50ba3f
Vinderosv <Vinderosv@yandex.by>
2021-10-23 23:31:29 +0300

    ГОТОВО

--- 09_ArraysAndCollections/homework_9.2/TodoList/src/main/java/Main.java
+++ 09_ArraysAndCollections/homework_9.2/TodoList/src/main/java/Main.java
@@ -3,58 +3,46 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
-    private static TodoList todoList = new TodoList();
+    private final static TodoList todoList = new TodoList();
 
     public static void main(String[] args) {
         while (true){
             Scanner scanner = new Scanner(System.in);
             String s  = scanner.nextLine();
 
-            final Pattern pattern = Pattern.compile("(LIST|ADD|DELETE|EDIT)\\s?(\\d*)\\s?(.*)");
+            final Pattern pattern = Pattern.compile("(LIST|ADD|DELETE|EDIT)\\s(\\d*)\\s?(.*)");
             Matcher matcher = pattern.matcher(s);
-            if (!matcher.matches()){
+            if (!matcher.lookingAt()){
                 System.out.println("Введена некорректная команда!");
+                break;
             }
-            while (matcher.matches()){
+            while (matcher.lookingAt()){
                 String command = matcher.group(1);
                 int index = !matcher.group(2).equals("") ? Integer.parseInt(matcher.group(2)) : -1;
                 String text = matcher.group(3);
-                boolean m = index <= matcher.groupCount();
                 if (command.equals("ADD")){
                     if (index > 0){
                         todoList.add(index,text);
-                        System.out.println("Дело добавлено по указанному индексу - " + index);
+                        System.out.println("Дело \"" + matcher.group(3) + "\" добавлено!");
                         break;
                     }else {
                         todoList.add(matcher.group(3));
-                        System.out.println("Дело добавлено");
+                        System.out.println("Дело \"" + matcher.group(3) + "\" добавлено!");
                         break;
                     }
 
                 }
                 else if (matcher.group(1).equals("EDIT")){
-                    if (m) {
                         todoList.edit(text, (index));
-                        System.out.println("Редактирование дела произошло успешно!");
-                    }else {
-                        System.out.println("Дела " + index + " не существует, его изменить нельзя!");
-                        System.out.println(args.length);
-                    }
                     break;
                 }
 
                 else if (matcher.group(1).equals("DELETE")) {
-
-                    if (m) {
                         todoList.delete(index);
-                        System.out.println("Дело " + index + " удалено успешно!");
-                    }else {
-                        System.out.println("Дела " + index + " не существует!");
-                    }
-                    break;
+                        break;
                 }
                 else if (matcher.group(1).equals("LIST")){
-                    System.out.println(todoList.getTodos());
+                    todoList.getTodos();
                     break;
                 }
             }
--- 09_ArraysAndCollections/homework_9.2/TodoList/src/main/java/TodoList.java
+++ 09_ArraysAndCollections/homework_9.2/TodoList/src/main/java/TodoList.java
@@ -23,21 +23,28 @@ public class TodoList {
         // TODO: заменить дело на index переданным todo индекс,
         //  проверьте возможность изменения
         if (index < list.size()&&index >= 0){
+            System.out.println("Дело \""+ list.get(index)+"\" заменено на \""+ todo + "\"");
             list.set(index,todo);
+        }else {
+            System.out.println("Дело с таким номером не существует");
         }
     }
 
     public void delete(int index) {
-        // TODO: удалить дело находящееся по переданному индексу,
-        //  проверьте возможность удаления дела
         if (index < list.size()&&index >= 0){
+            System.out.println("Дело \""+ list.get(index)+"\" удалено!");
             list.remove(index);
+        }else {
+            System.out.println("Дело с таким номером не существует");
         }
     }
 
     public ArrayList<String> getTodos() {
         // TODO: вернуть список дел
-
-            return new ArrayList<>(list);
+            for (int i = 0; i < list.size();i++){
+                String text = list.get(i);
+                System.out.println(i + " - " + text);
+            }
+            return list;
         }
     }
