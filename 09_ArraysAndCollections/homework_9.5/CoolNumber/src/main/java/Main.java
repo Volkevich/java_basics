c9aa84ed92bad3e1ac5577501f71593d8c535629
Vinderosv <Vinderosv@yandex.by>
2021-11-11 23:10:34 +0300

    В процессе...

--- 09_ArraysAndCollections/homework_9.5/CoolNumber/src/main/java/Main.java
+++ 09_ArraysAndCollections/homework_9.5/CoolNumber/src/main/java/Main.java
@@ -13,11 +13,7 @@ public class Main {
     public static void main(String[] args) {
     CoolNumbers.generateCoolNumbers();
         Collections.sort(CoolNumbers.generateCoolNumbers());
-        long start = System.currentTimeMillis();
-        int index = Collections.binarySearch(CoolNumbers.generateCoolNumbers(), "A111BC197");
-        System.out.println(index);
-        long duration = System.currentTimeMillis() - start;
-        System.out.println("Номер по бинарному поиску найден за - " + duration);
+
 
     }
 }
