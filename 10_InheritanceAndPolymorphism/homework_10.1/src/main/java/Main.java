c2ad2f55edda650d4310f8d9230fc113c8f1eb61
Виталий <Vinderosv@yandex.by>
2021-11-18 11:06:05 +0300

    Осталось реализовать дополнительное задание, а также внешний вид настроить

--- 10_InheritanceAndPolymorphism/homework_10.1/src/main/java/Main.java
+++ 10_InheritanceAndPolymorphism/homework_10.1/src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         BankAccount accountMax = new BankAccount();
@@ -9,12 +11,31 @@ public class Main {
         DepositAccount depositAccMax = new DepositAccount();
         DepositAccount depositAccOleg = new DepositAccount();
 
-        System.out.println("Выберите одну из команд: ");
-
-
-        accountMax.put(10000);
-        accountMax.send(accountOleg,500);
-        System.out.println("Остаток на счёте - " + accountOleg.getAmount() + " руб.");
+        System.out.println("Выберите пользователя счёта: ");
+        System.out.println("1. Максим");
+        System.out.println("2. Олег");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        int numberName = Integer.valueOf(name);
+        switch (numberName){
+            case 1 :
+                System.out.println(" Вы выбрали счёт пользователя Максим, его баланс - " + accountMax.getAmount());
+                System.out.println(" Выберите необходимый счёт: ");
+                System.out.println("1. Карт счет");
+                System.out.println("2. Депозитный счет");
+                String chet = scanner.nextLine();
+                int numberChet = Integer.valueOf(chet);
+                switch (numberChet){
+                    case 1:
+                        System.out.println("Вы выбрали карт счёт");
+                        System.out.println("Выберите необходимую операцию: ");
+                        System.out.println("1. Пополнение счета");
+                        System.out.println("2. Списание денежных средств");
+                        System.out.println("3. Перевод денежных средств");
+                }
+            case 2 :
+                System.out.println(accountOleg.getAmount() + " остаток на счету Олега");
+        }
     }
 
 }
