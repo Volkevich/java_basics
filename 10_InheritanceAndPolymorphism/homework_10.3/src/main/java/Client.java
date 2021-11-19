bad8906726a58f4a085937ed9948cd50607b379d
Vinderosv <Vinderosv@yandex.by>
2021-11-20 00:05:56 +0300

    Готово

--- 10_InheritanceAndPolymorphism/homework_10.3/src/main/java/Client.java
+++ 10_InheritanceAndPolymorphism/homework_10.3/src/main/java/Client.java
@@ -1,10 +1,10 @@
 public abstract class Client {
+
     protected double check;
 
     public abstract double getAmount();
 
     public abstract void put(double amountToPut);
-
 
     public abstract void take(double amountToTake);
 
