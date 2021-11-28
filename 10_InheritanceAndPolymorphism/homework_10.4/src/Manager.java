1d5165c94e9e6d7e1cf0a0831880a0ce03243107
Виталий Власов <vinderosv@yandex.by>
2021-11-28 10:07:22 +0000

    Исправлены методы, произведена корректировка

--- 10_InheritanceAndPolymorphism/homework_10.4/src/Manager.java
+++ 10_InheritanceAndPolymorphism/homework_10.4/src/Manager.java
@@ -1,17 +1,14 @@
 public class Manager extends Staff {
 
     protected static int FIX_SALARY = 58000;
-    protected static int SALARY_MANAGER = FIX_SALARY + getBONUS_MANAGER();
 
 
-    public static int getBONUS_MANAGER() {
-        return BONUS_MANAGER;
-    }
-    public static int getFIX_SALARY(){
-        return SALARY_MANAGER;
+    public int getSalary() {
+        return FIX_SALARY + getBONUS();
     }
 
-    public int getMonthSalary() {
-        return SALARY_MANAGER;
+    @Override
+    public int getBONUS() {
+        return (int) (Math.random() * (145000 - 115000)) + 115000;
     }
 }
