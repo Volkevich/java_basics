6d916b0a2bfea6a2d3b61432bfe7e04f22df8898
Виталий Власов <vinderosv@yandex.by>
2021-11-28 10:08:24 +0000

    Update TopManager.java

--- 10_InheritanceAndPolymorphism/homework_10.4/src/TopManager.java
+++ 10_InheritanceAndPolymorphism/homework_10.4/src/TopManager.java
@@ -1,12 +1,15 @@
 public class TopManager extends Staff {
     protected static int FIX_SALARY = 58000;
 
-    public int getBONUS_TOP_MANAGER() {
-        return BONUS_TOP_MANAGER;
+
+
+    @Override
+    public int getSalary() {
+        return FIX_SALARY + getBONUS();
     }
 
     @Override
-    public int getMonthSalary() {
-        return getSALARY_TOP_MANAGER();
+    public int getBONUS() {
+        return Company.getIncome() > 10_000_000 ? (int) (FIX_SALARY * 1.5) : 0;
     }
 }
