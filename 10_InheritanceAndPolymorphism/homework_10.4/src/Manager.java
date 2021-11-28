291991c5304e233a66b7a4f90c2782761ac7ef10
Виталий Власов <vinderosv@yandex.by>
2021-11-28 11:26:15 +0000

    Update Manager.java

--- 10_InheritanceAndPolymorphism/homework_10.4/src/Manager.java
+++ 10_InheritanceAndPolymorphism/homework_10.4/src/Manager.java
@@ -1,14 +1,18 @@
 public class Manager extends Staff {
 
-    protected static int FIX_SALARY = 58000;
+    protected static final int FIX_SALARY = 58000;
+    private final int salaryManager = FIX_SALARY + getBonus();
 
+    public Manager(Company company) {
+        super(company);
+    }
 
     public int getSalary() {
-        return FIX_SALARY + getBONUS();
+        return salaryManager;
     }
 
     @Override
-    public int getBONUS() {
+    public int getBonus() {
         return (int) (Math.random() * (145000 - 115000)) + 115000;
     }
 }
