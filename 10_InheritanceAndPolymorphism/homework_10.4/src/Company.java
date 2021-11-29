aa1d1ab2d104ebc7d6875123e1fe48b27b7e4581
Виталий Власов <vinderosv@yandex.by>
2021-11-29 18:42:17 +0000

    Update Company.java

--- 10_InheritanceAndPolymorphism/homework_10.4/src/Company.java
+++ 10_InheritanceAndPolymorphism/homework_10.4/src/Company.java
@@ -1,6 +1,7 @@
 import java.util.*;
 
 public class Company {
+
     private final List<Employee> employees = new ArrayList<>(); //список сотрудников
 
 
@@ -16,8 +17,15 @@ public class Company {
         this.employees.remove(employee);
     }
 
-    protected static int getIncome() { //значение дохода компании
-        return 0;
+    protected int getIncome() { //значение дохода компании
+
+        return 100000011 ;
+    }
+    protected int getTopManagerPercent(){
+        return (int) 1.5;
+    }
+    protected double getManagerPercent(){
+        return 0.5;
     }
 
     protected List<Employee> getFilteredLimitedList(int count, Comparator<Employee> employeeComparator) {
@@ -47,6 +55,31 @@ public class Company {
     }
     protected List<Employee> getEmployees(){
         return employees;
+    }
+    protected static void hireEmployees(Company company) {
+        for (int i = 0; i < 180; i++) {
+            Employee operator = new Operator(company);
+            company.hire(operator);
+        }
+        for (int i = 0; i < 80; i++) {
+            Employee manager = new Manager(company);
+            company.hire(manager);
+        }
+        for (int i = 0; i < 10; i++) {
+            Employee topManager = new TopManager(company);
+            company.hire(topManager);
+        }
+        System.out.println("Добавлено сотрудников компании: " + company.countEmployees());
+
+    }
+    protected static void fireHalfEmployees(Company company) {
+        int countEmployees = company.countEmployees();
+        for (int i = 0; i < countEmployees / 2; i++) {
+            int index = (int) (Math.random() * company.countEmployees());
+            Employee loser = company.getEmployees().get(index);
+            company.fire(loser);
+        }
+        System.out.println("Уволено : " + countEmployees / 2 + " сотрудников");
     }
 
 
