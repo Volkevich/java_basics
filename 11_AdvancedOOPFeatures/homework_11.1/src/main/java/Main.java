bb8b02c36ee82487d2bbedb98d81396c3909bcd4
Виталий <Vinderosv@yandex.by>
2021-11-30 16:47:10 +0300

    Реализован метод сортировки с помощью sort

--- 11_AdvancedOOPFeatures/homework_11.1/src/main/java/Main.java
+++ 11_AdvancedOOPFeatures/homework_11.1/src/main/java/Main.java
@@ -1,4 +1,11 @@
+import org.apache.commons.collections.list.TreeList;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+
+import static java.util.Comparator.reverseOrder;
 
 public class Main {
 
@@ -6,10 +13,19 @@ public class Main {
 
     public static void main(String[] args) {
         List<Employee> staff = Employee.loadStaffFromFile(STAFF_TXT);
-        System.out.println(staff);
+        sortBySalaryAndAlphabet(staff);
+
+
     }
 
     public static void sortBySalaryAndAlphabet(List<Employee> staff) {
-        //TODO Метод должен отсортировать сотрудников по заработной плате и алфавиту.
+        staff.sort(Comparator.comparing(Employee::getName));
+        staff.sort(Comparator.comparingInt(Employee::getSalary));
+
+        for (Employee employee : staff) {
+            System.out.println(employee);
+        }
+
     }
+    //TODO Метод должен отсортировать сотрудников по заработной плате и алфавиту.
 }
