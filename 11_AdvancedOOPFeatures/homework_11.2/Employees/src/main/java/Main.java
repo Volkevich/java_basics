e8a93c7bf53307b062c0afded7507d699396a9e5
Виталий Власов <vinderosv@yandex.by>
2021-12-04 22:23:48 +0000

    Реализован метод toLocalDate для преобразования Date к новому формату localDate и дальнейшего использования для сравнения по году

--- 11_AdvancedOOPFeatures/homework_11.2/Employees/src/main/java/Main.java
+++ 11_AdvancedOOPFeatures/homework_11.2/Employees/src/main/java/Main.java
@@ -1,4 +1,6 @@
-import java.util.List;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.*;
 
 public class Main {
 
@@ -8,11 +10,20 @@ public class Main {
         List<Employee> staff = Employee.loadStaffFromFile(STAFF_TXT);
         Employee employeeMaxSalary = findEmployeeWithHighestSalary(staff, 2017);
         System.out.println(employeeMaxSalary);
+
     }
 
     public static Employee findEmployeeWithHighestSalary(List<Employee> staff, int year) {
-        //TODO Метод должен вернуть сотрудника с максимальной зарплатой среди тех,
-        // кто пришёл в году, указанном в переменной year
-        return null;
+        return staff.stream()
+                .filter(employee -> toLocalDate(employee.getWorkStart()).getYear() == year)
+                .max(Comparator.comparing(Employee::getSalary))
+                .get();
     }
+
+    private static LocalDate toLocalDate(Date date) {
+        return date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+    }
+
+
+
 }
