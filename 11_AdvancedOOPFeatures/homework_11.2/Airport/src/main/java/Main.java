b2a8fda1a294c66081932078bdaff8a7cace3470
Виталий Власов <vinderosv@yandex.by>
2021-12-09 18:33:30 +0000

    Исправил метод, добавил IsBefore и isAfter для сравнения времени, так будет правильней и не будет бага

--- 11_AdvancedOOPFeatures/homework_11.2/Airport/src/main/java/Main.java
+++ 11_AdvancedOOPFeatures/homework_11.2/Airport/src/main/java/Main.java
@@ -1,36 +1,39 @@
 import com.skillbox.airport.Airport;
 import com.skillbox.airport.Flight;
-import com.skillbox.airport.Terminal;
+import net.sf.saxon.trans.SymbolicName;
+
 
 import java.time.*;
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
+
 
 public class Main {
     public static void main(String[] args) {
         findPlanesLeavingInTheNextTwoHours(Airport.getInstance());
-        System.out.println(findPlanesLeavingInTheNextTwoHours(Airport.getInstance()));
-        
-        
+        /*System.out.println(findPlanesLeavingInTheNextTwoHours(Airport.getInstance()));*/
+
+
 
     }
 
     public static List<Flight> findPlanesLeavingInTheNextTwoHours(Airport airport) {
-
         LocalDateTime now = LocalDateTime.now();
+        LocalDateTime twoTime = now.plusHours(2);
+        LocalDateTime minusTime = now.minusHours(2);
+
 
         List<Flight> flightList = airport.getTerminals().stream().flatMap(terminal -> terminal.getFlights()
                 .stream().filter(flight -> flight.getType()
                 .equals(Flight.Type.DEPARTURE)))
-                .filter(flight -> localDateTime(flight.getDate()).getHour() <= (now.plusHours(2).getHour()))
-                .filter(flight -> localDateTime(flight.getDate()).getHour()  >= (now.minusHours(2).getHour()))
+                .filter(flight -> localDateTime((flight.getDate())).isBefore(twoTime))
+                .filter(flight -> localDateTime(flight.getDate()).isAfter(minusTime))
                 .collect(Collectors.toList());
 
 
-        //TODO Метод должден вернуть список рейсов вылетающих в ближайшие два часа.
+        //TODO Метод должен вернуть список рейсов вылетающих в ближайшие два часа.
         return flightList;
     }
 
