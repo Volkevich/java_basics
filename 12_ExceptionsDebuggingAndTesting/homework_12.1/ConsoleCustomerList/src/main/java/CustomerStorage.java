75783a6bbc2ac70b0b3c43eb3afba7dac22a0ecc
Vinderosv <Vinderosv@yandex.by>
2021-12-20 12:29:53 +0300

    Добавлено, tru catch в маине, а также в методе CustomerStorage индивидуально throw new реализован

--- 12_ExceptionsDebuggingAndTesting/homework_12.1/ConsoleCustomerList/src/main/java/CustomerStorage.java
+++ 12_ExceptionsDebuggingAndTesting/homework_12.1/ConsoleCustomerList/src/main/java/CustomerStorage.java
@@ -13,9 +13,23 @@ public class CustomerStorage {
         final int INDEX_SURNAME = 1;
         final int INDEX_EMAIL = 2;
         final int INDEX_PHONE = 3;
+        String regexMail = "[A-Za-z]+@[a-z]+.[a-z]+";
+        String regexPhone = "[\\+][0-9]{11}";
 
         String[] components = data.split("\\s+");
+
+        if (components.length != 4) {
+            throw new IllegalArgumentException("Wrong Format: add Василий Петров \" +\n" +
+                    "\"vasily.petrov@gmail.com +79215637722\"");
+        }
         String name = components[INDEX_NAME] + " " + components[INDEX_SURNAME];
+
+        if (!components[INDEX_PHONE].matches(regexPhone)) {
+            throw new IllegalArgumentException("WRONG PHONE!!!!");
+        }
+        if (!components[INDEX_EMAIL].matches(regexMail)) {
+            throw new IllegalArgumentException("WRONG EMAIL!!!!");
+        }
         storage.put(name, new Customer(name, components[INDEX_PHONE], components[INDEX_EMAIL]));
     }
 
