b71e10f114de64b2414c3aac199140117ffa9029
Vinderosv <Vinderosv@yandex.by>
2022-01-05 22:53:05 +0300

    На данный момент необходимо нарисовать схему метро, при его соединении с этим у меня сейчас трудности

--- 12_ExceptionsDebuggingAndTesting/homework_12.2/SPBMetro/src/test/java/RouteCalculatorTest.java
+++ 12_ExceptionsDebuggingAndTesting/homework_12.2/SPBMetro/src/test/java/RouteCalculatorTest.java
@@ -4,65 +4,87 @@ import junit.framework.TestCase;
 import org.junit.Test;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class RouteCalculatorTest extends TestCase {
 
 
-    List<Station> route;
+    List<Station> forTransferRoute;
+    List<Station> TransferRoute;
+
+
     RouteCalculator routeCalculator;
-    static StationIndex stationIndex = new StationIndex();
-    Station station1;
-    Station station2;
-    Station station3;
+    StationIndex stationIndex;
+
+    Station altaiskay;
+    Station pushkinskaya;
+    Station zimnya;
+    Station summer;
 
     List<Station> connect;
 
     @Override
     protected void setUp() throws Exception {
-        route = new ArrayList<>();
+        forTransferRoute = new ArrayList<>();
+
+
+        stationIndex = new StationIndex();
+
+        Line redLine = new Line(1, "Красная");
+        Line blueLine = new Line(2, "Синяя");
+        Line greenLine = new Line(3, "Зеленая");
+
+
+        forTransferRoute.add(new Station("Петровская", redLine));
+        forTransferRoute.add(new Station("Пушкинская", redLine));
+        forTransferRoute.add(new Station("Молодежная", redLine));
+        forTransferRoute.add(new Station("Грушевка", blueLine));
+
+
+        //TODO:Станции
+        altaiskay = new Station("Алтайская", blueLine);
+        pushkinskaya = new Station("Пушкинская", redLine);
+        zimnya = new Station("Зимняя", greenLine);
+        summer = new Station("Летняя", greenLine);
+        Station sun = new Station("Солнечная", redLine);
+        Station redgor = new Station("Красногорская", redLine);
+        Station blueEye = new Station("Синевокая", redLine);
+        Station pear = new Station("Грушевка", blueLine);
+        Station youth = new Station("Молодежная", redLine);
+
+
+
+
+
+        Stream.of(redLine, blueLine, greenLine).forEach(stationIndex::addLine);
+        Stream.of(altaiskay, pushkinskaya, zimnya, summer,sun,redgor,blueEye,pear,youth).
+                peek(station -> station.getLine().addStation(station)).
+                forEach(stationIndex::addStation);
+        stationIndex.addConnection(Stream.of(altaiskay,pushkinskaya).collect(Collectors.toList()));
+        stationIndex.addConnection(Stream.of(zimnya,summer).collect(Collectors.toList()));
+        stationIndex.getConnectedStations(altaiskay);
+        stationIndex.getConnectedStations(zimnya);
+
         routeCalculator = new RouteCalculator(stationIndex);
 
-
-        Line line1 = new Line(1, "Красная");
-        Line line2 = new Line(2, "Синяя");
-        Line line3 = new Line(3, "Зеленая");
-        stationIndex.addLine(line1);
-        stationIndex.addLine(line2);
-        stationIndex.addLine(line3);
-
-        line1.addStation(new Station("Алтайская", line1));
-        line1.addStation(new Station("Каменская", line1));
-        line1.addStation(new Station("Алтайская", line1));
-
-        line3.addStation(new Station("Нагорная", line3));
-        line3.addStation(new Station("Зимняя", line3));
-        line3.addStation(new Station("Красивая", line3));
-
-
-        route.add(new Station("Петровская", line1));
-        route.add(new Station("Пушкинская", line1));
-        route.add(new Station("Петровщина", line1));
-        route.add(new Station("Грушевка", line2));
-
-        station1 = new Station("Алтайская", line2);
-        station2 = new Station("Пушкинская", line1);
-        station3 = new Station("Зимняя", line3);
-
+        TransferRoute = Stream.of(altaiskay,sun,zimnya).collect(Collectors.toList());
 
     }
+
     @Test
     public void testCalculateDuration() {
-        double actual = RouteCalculator.calculateDuration(route);
+        double actual = RouteCalculator.calculateDuration(forTransferRoute);
         double expected = 8.5;
         assertEquals(expected, actual);
     }
+
     @Test
     public void testShortestRoute() {
-        List<Station> actual = routeCalculator.getShortestRoute(station1, station3);
-        List<Station> expected = new ArrayList<Station>(Arrays.asList(station1, station2,station3));
-        assertEquals(expected, actual);
+    List<Station> actualNotransfer = routeCalculator.getShortestRoute(altaiskay,zimnya);
+    List<Station> expectedNoTransfer = TransferRoute;
+        assertEquals(expectedNoTransfer, actualNotransfer);
     }
 
 
