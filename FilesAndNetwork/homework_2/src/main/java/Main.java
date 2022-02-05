a8758dad5f1a893f3ea8a0eddce97ed894455afe
Dssa95 <87798074+Dssa95@users.noreply.github.com>
2022-02-05 22:38:05 +0300

    Update Main.java

--- FilesAndNetwork/homework_2/src/main/java/Main.java
+++ FilesAndNetwork/homework_2/src/main/java/Main.java
@@ -1,4 +1,15 @@
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, URISyntaxException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите абсолютный путь к папке которую хотите скопировать: ");
+        String sourceDir = scanner.nextLine();
+        System.out.println("Введите абсолютный путь куда хотите скопировать папку: ");
+        String sourceDirOut = scanner.nextLine();
+        FileUtils.copyFolder(sourceDir,sourceDirOut);
     }
 }
