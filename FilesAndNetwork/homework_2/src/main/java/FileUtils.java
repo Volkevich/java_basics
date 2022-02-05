dc074316016afa983a10a5631f1dffd4225c3972
Dssa95 <87798074+Dssa95@users.noreply.github.com>
2022-02-05 22:37:51 +0300

    Update FileUtils.java

--- FilesAndNetwork/homework_2/src/main/java/FileUtils.java
+++ FilesAndNetwork/homework_2/src/main/java/FileUtils.java
@@ -1,5 +1,29 @@
+import java.io.*;
+import java.nio.file.*;
+import java.util.Objects;
+
+
 public class FileUtils {
-    public static void copyFolder(String sourceDirectory, String destinationDirectory) {
-        // TODO: write code copy content of sourceDirectory to destinationDirectory
+    public static void copyFolder(String sourceDirectory, String destinationDirectory) throws IOException {
+        File fileSource = Paths.get(sourceDirectory).toFile();
+        File fileDir = Paths.get(destinationDirectory).toFile();
+
+        if (fileSource.isDirectory()) {
+            copyDirect(fileSource,fileDir);
+        }else {
+            Files.copy(Path.of(sourceDirectory),Path.of(destinationDirectory));
+        }
+
+    }
+    private static void copyDirect(File sourceDir, File targetDir) throws IOException{
+        if (!targetDir.exists()){
+            Files.createDirectories(targetDir.toPath());
+        }
+
+        for (String s : Objects.requireNonNull(sourceDir.list())){
+            File file = new File(sourceDir,s);
+            File file1 = new File(targetDir,s);
+            copyFolder(file.getAbsolutePath(),file1.getAbsolutePath());
+        }
     }
 }
