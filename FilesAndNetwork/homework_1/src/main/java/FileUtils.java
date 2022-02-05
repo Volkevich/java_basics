0101f7d83fbc0fb11ec4222694b86bbe88ea0a0f
Dssa95 <87798074+Dssa95@users.noreply.github.com>
2022-02-05 22:37:04 +0300

    Update FileUtils.java

--- FilesAndNetwork/homework_1/src/main/java/FileUtils.java
+++ FilesAndNetwork/homework_1/src/main/java/FileUtils.java
@@ -1,6 +1,14 @@
-public class FileUtils {
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
 
-    public static long calculateFolderSize(String path) {
-        return 0;
+public class FileUtils extends SimpleFileVisitor<Path> {
+    public static long calculateFolderSize(String path) throws IOException {
+        Path folder = Paths.get(path);
+        final long allSize = Files.walk(folder).map(Path::toFile).filter(File::isFile).mapToLong(File::length).sum();
+        return allSize;
     }
 }
